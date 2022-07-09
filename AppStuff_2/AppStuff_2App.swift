bc9778bba550f95910e4d21bf072c9d21d81d4c2
kxx <132kxx@gmail.com>
2022-07-09 10:16:55 +0900

    Initial Commit

---
+++ AppStuff_2/AppStuff_2App.swift
@@ -0,0 +1,17 @@
+//
+//  AppStuff_2App.swift
+//  AppStuff_2
+//
+//  Created by kxx: on 2022/07/09.
+//
+
+import SwiftUI
+
+@main
+struct AppStuff_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStuff_2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppStuff_2
+//
+//  Created by kxx: on 2022/07/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
